322239af4d29574e34ba24aee253608749b17b38
HarshanAruchami <harshanaruchami@gmail.com>
2023-09-08 17:07:52 +0530

    yet to complete

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,80 @@
+//Navbar Scrolldown BG-color Changes
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+//LAPTOP VIEW
+//baner content
 $(document).ready(function () {
-  $("#f-col").waypoint(
-    function () {
-      $("#f-col,#right,#register,.navbar-toggler ").addClass(
-        "animate__animated animate__fadeInRight"
-      );
-      $("#s-col,.navbar-brand").addClass(
-        "animate__animated animate__fadeInLeft"
-      );
-      $("#top").addClass("animate__animated animate__fadeInUp");
-    },
-    { offset: "50%" }
-  );
+  if (window.innerWidth > 991) {
+    $("#f-col").waypoint(
+      function () {
+        $("#home-bg-video").addClass("animate__animated animate__zoomIn");
+        $("#f-col,#right,#register,.navbar-toggler ").addClass(
+          "animate__animated animate__fadeInRight"
+        );
+        $("#s-col,.navbar-brand").addClass(
+          "animate__animated animate__fadeInLeft"
+        );
+        $("#top").addClass("animate__animated animate__fadeInUp");
+      },
+      { offset: "50%" }
+    );
+  }
+});
+
+//cards
+$(document).ready(function () {
+  if (window.innerWidth > 768) {
+    $(".card-border1").waypoint(
+      function () {
+        $("#top,#right,#register,.navbar-toggler,#f-col ").addClass(
+          "animate__animated animate__fadeInRight"
+        );
+        $(".navbar-brand,#s-col").addClass(
+          "animate__animated animate__fadeInLeft"
+        );
+        $(".card-border1,.card-border2,.card-border3,.card-border4").addClass(
+          "animate__animated animate__fadeInUp"
+        );
+      },
+      { offset: "100%" }
+    );
+  }
+});
+
+//MOBILE VIEW
+$(document).ready(function () {
+  if (window.innerWidth <= 768) {
+    $(".navbar-toggler").waypoint(
+      function () {
+        $("#top,#right,#register,.navbar-toggler,#f-col ").addClass(
+          "animate__animated animate__fadeInRight"
+        );
+        $(".navbar-brand,#s-col").addClass(
+          "animate__animated animate__fadeInLeft"
+        );
+        $(".card-border1,.card-border2,.card-border3,.card-border4").addClass(
+          "animate__animated animate__fadeInUp"
+        );
+      },
+      { offset: "10%" }
+    );
+  }
 });
 
 $(document).ready(function () {
-  $(".card-border").waypoint(
-    function () {
-      $(".card-border").addClass("animate__animated animate__fadeInUp");
-    },
-    { offset: "100%" }
-  );
+  if (window.innerWidth <= 575) {
+    $(".navbar-toggler").waypoint(
+      function () {
+        $(".navbar-brand,#f-col").addClass(
+          "animate__animated animate__fadeInUp"
+        );
+      },
+      { offset: "10%" }
+    );
+  }
 });
